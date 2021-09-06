bbc7eb088c7a97affdb569474d400f777fd833f9
Kinasha <znella@foxmail.com>
2021-09-06 08:57:00 +0800

    chore test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,16 @@
 module.exports = {
     lang: 'zh-CN',
-    title: '你好， VuePress ！',
-    description: '这是我的第一个 VuePress 站点',
-    themeConfig: {
-        logo: 'https://vuejs.org/images/logo.png',
-    },
+    title: 'Kinasha 的前端博客',
+    description: 'Kinasha 的前端博客',
+    // themeConfig: {
+    //     logo: 'https://vuejs.org/images/logo.png',
+    //     // nav: [
+    //     //     {
+    //     //         text: '前端游乐场',
+
+    //     //     },
+    //     //     {
+    //     //         text: '杂记',
+    //     //     }],
+    // },
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,14 @@
-# Hello VuePress
+---
+home: true
+title: Kinasha
+heroText: Kinasha
+tagline: Heigher an heigher
+actionText: 前端博客 →
+actionLink: /frontend/index.html
+features:
+- title: About Me
+  details: Mechanical Student,Self-learner and ByteDancer 
+- title: My wechat
+  details: AllenZhangweicheng
+footer: MIT Licensed | Copyright © 2021-present Kinasha
+---
