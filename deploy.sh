7a1e5369ec3facbe70f4131231bea2882e4e22e2
Kinasha <znella@foxmail.com>
2021-09-06 09:12:09 +0800

    test deploy

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:kinasha/kinasha.github.io.git main
+git push -f git@github.com:Kinasha/Kinasha.github.io.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
