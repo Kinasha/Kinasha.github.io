e91ef302e7c434e4f4ecfe8035f783f4f954670c
zhangweicheng <www.zhangweicheng01@corp.netease.com>
2023-04-02 16:23:43 +0800

    🎨 avatar

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./index.css";
+import styles from "./styles.module.css";
 const defaultAvatar =
   "https://p5.music.126.net/obj/wo3DlcOGw6DClTvDisK1/26356132181/372b/c442/68e9/75744a5d6251cf62830a7db036f7ddd9.png";
 export default function ({ url = defaultAvatar }) {
   return (
-    <img className="hero_avatar" src={url} alt="a random avatar picture" />
+    <img className={styles.avatar} src={url} alt="a random avatar picture" />
   );
 }
